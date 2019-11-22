134d5440c400c7da5fab89bd5b98c06f48825011
Pedro Lourenco <dev.pedrolourenco@gmail.com>
2019-11-22 14:34:06 -0300

    Initial commit

---
+++ include/banco.php
@@ -0,0 +1,20 @@
+<?php 
+// renomeie o nome "helpdesk2" para o nome que vc vai dar para o banco.
+
+   
+$con =@ mysqli_connect("localhost","root","admin","helpdesk2") or die(mysqli_connect_errno());
+	
+
+/*	$host="localhost";
+	$user="root";
+	$pass="admin";
+	$dbname="helpdesk2";
+
+	$con=mysqli_connect($host,$user,$pass); */
+
+
+	
+
+
+?>
+
